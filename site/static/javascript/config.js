55e1237bcdd1c8aa162a889b4acab09cc253bf2a
Syed Shahriyar Ali <70702829+SyedShahriyarAli@users.noreply.github.com>
2022-03-07 12:59:55 +0500

    Update config.js

--- site/static/javascript/config.js
+++ site/static/javascript/config.js
@@ -1,1 +1,1 @@
-var SubDomain = 'aku'; var SiteID = '6D3F2C5A-9B2D-485F-B9E6-AF8D7594A12B'; var host = 'https://api.eratest.site';
+var SubDomain = 'aku'; var SiteID = '6D3F2C5A-9B2D-485F-B9E6-AF8D7594A12B'; var host = 'http://20.127.3.14/ERAAPI';
